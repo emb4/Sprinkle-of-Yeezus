cc255c947329da2a065f1e54eda87bc17ed21763
Eric Bates <ericbates@erics-imac.local>
2018-05-01 19:58:52 -0400

    Initial Commit

---
+++ Sprinkle of Yeezus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sprinkle of Yeezus
+//
+//  Created by Eric Bates on 5/1/18.
+//  Copyright © 2018 Eric Bates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

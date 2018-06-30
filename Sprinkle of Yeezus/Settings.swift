322d3f9f300ee8355ca9d2540090a6ca5c6c4f8c
Eric Bates <ericbates@erics-imac.local>
2018-06-30 15:35:47 -0400

    Quote picking and notifications now avoid duplicates

--- Sprinkle of Yeezus/Settings.swift
+++ Sprinkle of Yeezus/Settings.swift
@@ -15,12 +15,9 @@ class SettingsPage: UIViewController {
     @IBOutlet private weak var informationLabel: UILabel!
     @IBOutlet private weak var changeTimeButton: UIButton!
     @IBOutlet private weak var notificationSwitch: UISwitch!
-    @IBOutlet private weak var timePicked: UIDatePicker!
     
     private var sprinkleTimePicked = Defaults.notificationDate ?? Date()
     private let notificationCenter = UNUserNotificationCenter.current()
-    
-
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -30,12 +27,13 @@ class SettingsPage: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        //Loads saved time from datepicker when getting to view
         let labelTime = UserDefaults.standard.string(forKey: "savedLabel")
         informationLabel.text = labelTime
     }
     
     // MARK: - IBAction
-    @IBAction private func NotificationSwitch(_ sender: UISwitch) {
+    @IBAction private func notificationSwitch(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn, forKey: "switchStatus") //saves the status of the switch
         if sender.isOn {
             notificationCenter.requestAuthorization(options: [.alert, .sound], completionHandler: { didAllow, error in
@@ -45,16 +43,14 @@ class SettingsPage: UIViewController {
                 }
             })
         }
-        
         updateUI()
     }
     
     @IBAction private func pickTimeAction() {
+        //Run when time is set on date picker
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        
         let controller = storyboard.instantiateViewController(withIdentifier: "TimePickerViewController") as! TimePicker
         controller.delegate = self
-        
         navigationController?.pushViewController(controller, animated: true)
     }  
     
@@ -63,39 +59,44 @@ class SettingsPage: UIViewController {
         [informationLabel, changeTimeButton].forEach { $0.isHidden = !notificationSwitch.isOn }
     }
     
+    
     private func sprinkleNotifications(_ sprinkleList: Array<Sprinkle>) {
         notificationCenter.removeAllPendingNotificationRequests()
         
-        guard notificationSwitch.isOn else {
+        guard notificationSwitch.isOn else { //Runs only user turns on notification switch
             return
         }
         
         let notification = UNMutableNotificationContent()
-        let notificationText = pickRandomQuote(sprinkleList, pickedCount)
-        
-        notification.body = "\"\(notificationText.quote)\""
-        if notificationText.quoteSource != "" {
-            notification.body.append(" - \(notificationText.quoteSource)")
-        }
-        if notificationText.date != 0 {
-            notification.body.append(", \(notificationText.date)")
-        }
-
-        var components = Calendar.current.dateComponents([.hour, .minute], from: sprinkleTimePicked)
+        var notificationText = [Sprinkle]()
+        var components = Calendar.current.dateComponents([.hour, .minute], from: sprinkleTimePicked) //grabs time from picker
         
         let today = Date()
         let calendar = Calendar.current
-        components.day = calendar.component(.day, from: today)
+        components.day = calendar.component(.day, from: today) //grabs current day from phones calendar
         let month = 30
         var nextDay = DateComponents()
         nextDay.hour = components.hour
         nextDay.minute = components.minute
         
+        
+        //schedules sprinkles for 30 days
        for days in 0...month {
-        nextDay.day = components.day! + days
+        notificationText.append(pickRandomQuote(sprinkleList))
+        
+        notification.body = "\"\(notificationText[days].quote)\"" //Formats notification
+        if notificationText[days].quoteSource != "" {
+            notification.body.append(" - \(notificationText[days].quoteSource)")
+        }
+        if notificationText[days].date != 0 {
+            notification.body.append(", \(notificationText[days].date)")
+        }
+        
+        nextDay.day = components.day! + days //tommorow is today plus 1 and the time set
         let trigger = UNCalendarNotificationTrigger(dateMatching: nextDay, repeats: false)
         let request = UNNotificationRequest(identifier: "Sprinkle number \(days)", content: notification, trigger: trigger)
         print("Time set for \(nextDay)")
+        print("Notification content: \(notificationText[days].quote)")
         
         notificationCenter.add(request) { error in
             if let error = error {
@@ -103,10 +104,7 @@ class SettingsPage: UIViewController {
                 }
             }
         }
-        
         updateLabelTime()
-        
-
     }
     
     func updateSwitchStatus(){ //this function checks to see if the switch was flipped in the past and acts accordingly
--- Sprinkle of Yeezus/ViewController.swift
+++ Sprinkle of Yeezus/ViewController.swift
@@ -9,39 +9,47 @@
 import UIKit
 import UserNotifications
 
-var pickedCount = Array(repeating: 0, count: sprinkleList.count)
+var elementPicked = [Int]() //array that keeps track of previously picked quotes to avoid duplicates
 
-func pickRandomQuote(_  sprinkleList: Array<Sprinkle>, _ pickrate: Array<Int>) -> Sprinkle{
+func pickRandomQuote(_  sprinkleList: Array<Sprinkle>) -> Sprinkle{
+    // Picks a random Kanye Quote from structure
+    print("\nPicking quote...")
     let range = sprinkleList.count
-    let pick = Int(arc4random_uniform(UInt32(range)))
-    
-    var minimum = pickrate.min()
+    var pick = Int(arc4random_uniform(UInt32(range)))
+    var notADuplicate = false
     var selection = sprinkleList[pick]
-    if selection.timesPicked == minimum!{
-        selection.timesPicked += 1
-        
-    }
+    var attempt = 0
     
-    print(sprinkleList[pick])
-    return sprinkleList[pick]
-    //This picks a random kanye quote from the structure
+    while notADuplicate == false { //avoids duplicate picks.. unless it can't fine a unique quote after 10 tries
+        if elementPicked.contains(pick){
+            if attempt == 10{
+                print("No unique quotes found after 10 attemps.. picking: \(selection.quote)")
+                return selection
+            }
+            attempt += 1
+            print("Duplicate found \(attempt) times.. trying again")
+            pick = Int(arc4random_uniform(UInt32(range)))
+            selection = sprinkleList[pick]
+        } else {
+            notADuplicate = true
+            print("Picked quote: \(selection.quote)")
+            elementPicked.append(pick)
+            return selection
+        }
+    }
 }
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var quoteLabel: UILabel!
-    @IBOutlet weak var NewQuoteButtonLabel: UIButton!
+    @IBOutlet weak var newQuoteButtonLabel: UIButton!
     @IBOutlet weak var sourceLabel: UILabel!
     
     //Filling in boxes
-    func fillTextBoxes(_ sprinkleList: Sprinkle) {
-        var sprinkleQuote = sprinkleList
+    func fillTextBoxes(_ sprinkleQuote: Sprinkle) {
         
         //fill quote box with quote
         quoteLabel.text = "\(sprinkleQuote.quote)"
-        sprinkleQuote.timesPicked += 1
-       // QuoteBox.centerVertically()
-        
         
         //fill source box
         if sprinkleQuote.quoteSource.isEmpty {
@@ -59,27 +67,22 @@ class ViewController: UIViewController {
             }
         }
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.4705882353, green: 0.9411764706, blue: 0.368627451, alpha: 1)
-        pickedCount[5] = 6
     }
     
-    @IBAction func NewQuoteButton(_ sender: UIButton) {
-        fillTextBoxes(pickRandomQuote(sprinkleList, pickedCount))
-        NewQuoteButtonLabel.setTitle("MORE KANYE-FIDENCE", for: .normal)
-       // DescLabel.isHidden = true
+    @IBAction func newQuoteButton(_ sender: UIButton) {
+        fillTextBoxes(pickRandomQuote(sprinkleList))
+        newQuoteButtonLabel.setTitle("MORE KANYE-FIDENCE", for: .normal)
     }
     
-    @IBAction func ShareButton(_ sender: UIButton) {
+    @IBAction func shareButton(_ sender: UIButton) {
         let quote = UIActivityViewController(activityItems: [quoteLabel.text! + " - via Sprinkle of Yeezus app"], applicationActivities: nil)
         present(quote, animated: true, completion: nil)
     }
-    
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
